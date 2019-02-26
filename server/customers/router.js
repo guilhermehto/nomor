25a8e6f371a210a5f495a11f1b924006288312f5
Guilherme Henrique Teixeira de Oliveira <htoguilherme@gmail.com>
2019-02-26 17:58:04 -0300

    Added customers PATCH

--- server/customers/router.js
+++ server/customers/router.js
@@ -10,7 +10,7 @@ const getCustomers = (req, res) => {
   })
 }
 
-const postCustomers = (req, res) => {
+const postCustomer = (req, res) => {
   const customerBody = _.pick(req.body, ['name', 'email'])
   const customer = new Customer(customerBody)
   customer.save().then(doc => {
@@ -20,7 +20,21 @@ const postCustomers = (req, res) => {
   })
 }
 
+const patchCustomer = (req, res) => {
+  const customerBody = _.pick(req.body, ['name', 'email'])
+  Customer.findByIdAndUpdate(req.params.id, { $set: customerBody }, {new: true}).then(customer => {
+    if (!customer) {
+      return res.status(404).send()
+    }
+
+    return res.send({customer})
+  }).catch(error => {
+    res.status(400).send(error)
+  })
+}
+
 router.get('/customers', getCustomers)
-router.post('/customers', postCustomers)
+router.post('/customers', postCustomer)
+router.patch('/customers/:id', patchCustomer)
 
 module.exports = router
