8b0713a2016b4756ff67cc72d8fcb92db8c76ca6
Guilherme Henrique Teixeira de Oliveira <htoguilherme@gmail.com>
2019-02-26 17:27:38 -0300

    Added customers POST

--- server/customers/router.js
+++ server/customers/router.js
@@ -1,5 +1,6 @@
 const Customer = require('./customer')
 const router = require('express').Router()
+const _ = require('lodash')
 
 const getCustomers = (req, res) => {
   Customer.find().then(customers => {
@@ -9,6 +10,17 @@ const getCustomers = (req, res) => {
   })
 }
 
+const postCustomers = (req, res) => {
+  const customerBody = _.pick(req.body, ['name', 'email'])
+  const customer = new Customer(customerBody)
+  customer.save().then(doc => {
+    res.send(doc)
+  }, error => {
+    res.status(400).send(error)
+  })
+}
+
 router.get('/customers', getCustomers)
+router.post('/customers', postCustomers)
 
 module.exports = router
