4ed58d16c511364077f05f8f54365065726202ff
Guilherme Henrique Teixeira de Oliveira <htoguilherme@gmail.com>
2019-02-27 17:47:57 -0300

    Hooked receiver routes to app

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+
 const mongoose = require('./db/mongoose')
+
 const customersRouter = require('./customers/router')
+const receiversRouter = require('./receiver/router')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 
-app.use('', require('./customers/router'))
+app.use('', customersRouter)
+app.use('', receiversRouter)
 
 app.listen(PORT, () => {
     console.log(`Started on port ${PORT}`)
