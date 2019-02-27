343790ce9ed7c8354cea2d657a9823e7b74df435
Guilherme Henrique Teixeira de Oliveira <htoguilherme@gmail.com>
2019-02-27 17:37:19 -0300

    Added receiver model

---
+++ server/receiver/receiver.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const ReceiverSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3
+  }, email: {
+    type: String,
+    minlength: 4,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  }
+})
+
+const Receiver = mongoose.model('Receiver', ReceiverSchema)
+
+module.exports = { Receiver, ReceiverSchema }
