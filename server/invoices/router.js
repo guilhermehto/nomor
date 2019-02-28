bce1511bbf391e988118809b2fc28b9deebd9ccc
Guilherme Henrique Teixeira de Oliveira <htoguilherme@gmail.com>
2019-02-28 14:53:32 -0300

    Added invoices /PATCH

--- server/invoices/router.js
+++ server/invoices/router.js
@@ -36,7 +36,43 @@ const postInvoices = (req, res) => {
   })
 }
 
+const patchInvoice = (req, res) => {
+  const invoiceBody = _.pick(req.body, ['description', 'amount', 'dueTo', 'paid', 'customer'])
+
+  if (!invoiceBody.customer) {
+    return saveInvoice(req.params.id, invoiceBody, res)
+  }
+
+  if (!ObjectID.isValid(invoiceBody.customer)) {
+      return res.status(400).send({ error: 'Invalid customer ID' })
+  }
+
+  mongoose.model('Customer').findById(invoiceBody.customer).then(customer => {
+    if (!customer) {
+      return res.status(404).send({ error: 'Customer not found' })
+    }
+
+    return saveInvoice(req.params.id, invoiceBody, res)
+  }, error => {
+    res.status(400).send(error)
+  })
+
+}
+
+const saveInvoice = (id, body, res) => {
+  return Invoice.findOneAndUpdate(id, { $set: body }, { new: true }).then(updatedInvoice => {
+    if (!updatedInvoice) {
+      return res.status(404).send()
+    }
+
+    return res.send(updatedInvoice)
+  }, error => {
+    return res.status(400).send(error)
+  })
+}
+
 router.get('/invoices', getInvoices)
 router.post('/invoices', postInvoices)
+router.patch('/invoices/:id', patchInvoice)
 
 module.exports = router
